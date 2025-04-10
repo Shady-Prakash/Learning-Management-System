a32e9b5958472f622cdea76d81c9e6d70290b9c8
Shady-Prakash <shadyprakash8@gmail.com>
2025-04-10 03:58:04 +0100

    comment users and roles code

--- app/(dashboard)/(routes)/admin/roles/_components/[roleId]/page.tsx
+++ app/(dashboard)/(routes)/admin/roles/_components/[roleId]/page.tsx
@@ -1,62 +1,62 @@
-import { auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
-import { CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-react";
+// import { auth } from "@clerk/nextjs";
+// import { redirect } from "next/navigation";
+// import { CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-react";
 
-import { db } from "@/lib/db";
-import { IconBadge } from "@/components/icon-badge";
-import { Banner } from "@/components/banner";
+// import { db } from "@/lib/db";
+// import { IconBadge } from "@/components/icon-badge";
+// import { Banner } from "@/components/banner";
 
-import { TitleForm } from "./_components/title-form";
+// import { TitleForm } from "./_components/title-form";
 
-const OrganizationIdPage = async ({
-  params
-}: {
-  params: { organizationId: string }
-}) => {
-  const { userId } = auth();
+// const OrganizationIdPage = async ({
+//   params
+// }: {
+//   params: { organizationId: string }
+// }) => {
+//   const { userId } = auth();
 
-  if(!userId) {
-    return redirect("/");
-  }
+//   if(!userId) {
+//     return redirect("/");
+//   }
 
-  const organization = await db.organization.findUnique({
-    where: {
-      id: params.organizationId,
-      userId
-    },
-  });
-
-
-  if(!organization) {
-    return redirect("/");
-  }
+//   const organization = await db.organization.findUnique({
+//     where: {
+//       id: params.organizationId,
+//       userId
+//     },
+//   });
 
 
-  return ( 
-    <>
-      {!organization && (
-        <Banner
-          label="This course is unpublished. It will not be visible to the students."
-        />
-      )}
-      <div className="p-6">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-          <div>
-            <div className="flex items-center gap-x-2">
-              <IconBadge icon={LayoutDashboard}/>
-              <h2 className="text-xl">
-                Customize your course
-              </h2>
-            </div>
-            <TitleForm
-              initialData={organization}
-              organizationId={organization.id}
-            />
-          </div>
-        </div>
-      </div>
-    </>
-   );
-}
+//   if(!organization) {
+//     return redirect("/");
+//   }
+
+
+//   return ( 
+//     <>
+//       {!organization && (
+//         <Banner
+//           label="This course is unpublished. It will not be visible to the students."
+//         />
+//       )}
+//       <div className="p-6">
+//         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+//           <div>
+//             <div className="flex items-center gap-x-2">
+//               <IconBadge icon={LayoutDashboard}/>
+//               <h2 className="text-xl">
+//                 Customize your course
+//               </h2>
+//             </div>
+//             <TitleForm
+//               initialData={organization}
+//               organizationId={organization.id}
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//    );
+// }
  
-export default OrganizationIdPage;
+// export default OrganizationIdPage;
--- app/(dashboard)/(routes)/admin/roles/page.tsx
+++ app/(dashboard)/(routes)/admin/roles/page.tsx
@@ -1,31 +1,31 @@
-'use client'
+// 'use client'
 
-import { useOrganization } from '@clerk/nextjs'
-import { DataTable } from './_components/data-table'
-import { columns } from './_components/columns'
-import SelectRole from './_components/select-role'
-import { OrgMembersParams } from '@/lib/organizations'
+// import { useOrganization } from '@clerk/nextjs'
+// import { DataTable } from './_components/data-table'
+// import { columns } from './_components/columns'
+// import SelectRole from './_components/select-role'
+// import { OrgMembersParams } from '@/lib/organizations'
 
-// List of organization memberships. Administrators can
-// change member roles or remove members from the organization.
-const ManageRoles = () => {
-  const { isLoaded, memberships } = useOrganization(OrgMembersParams)
+// // List of organization memberships. Administrators can
+// // change member roles or remove members from the organization.
+// const ManageRoles = () => {
+//   const { isLoaded, memberships } = useOrganization(OrgMembersParams)
 
-  const organizationUsers = memberships?.data?.map((mem) => {
-    return mem
-  })
+//   const organizationUsers = memberships?.data?.map((mem) => {
+//     return mem
+//   })
 
-  if (!isLoaded) {
-    return <>Loading</>
-  }
+//   if (!isLoaded) {
+//     return <>Loading</>
+//   }
 
-  return (
-    <div className="p-6">
-      <DataTable columns={columns} data={organizationUsers!}/>
-    </div>
-  )
-}
+//   return (
+//     <div className="p-6">
+//       <DataTable columns={columns} data={organizationUsers!}/>
+//     </div>
+//   )
+// }
 
-export default ManageRoles;
+// export default ManageRoles;
 
-<SelectRole/>
+// <SelectRole/>
